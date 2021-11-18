bef21614188cd21e97a3cd3c35f5c8a218b92420
pjblecha <44296698+pjblecha@users.noreply.github.com>
2021-11-17 18:34:19 -0600

    Committing changes, keeping Express (for now)

--- server.js
+++ server.js
@@ -16,15 +16,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
-
-/* Original code
-const express = require('express')
-const app = express();
-const port = process.env.PORT || 3000;
-
-const routes = require('./api/routes');
-routes(app);
-app.listen(port, function() {
-   console.log('Server started on port: ' + port);
-});
-*/
